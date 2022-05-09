5940bd59b098561fa2971801db2ba1db1a3071ca
anh nguyen <nguyenlamanh1701@gmail.com>
2022-05-09 21:32:53 +0700

    implement getAll fragment hítory

---
+++ app/src/main/java/com/example/kt2/dal/SQLiteHelper.java
@@ -0,0 +1,70 @@
+package com.example.kt2.dal;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.kt2.model.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SQLiteHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "ChiTieu1.db";
+    private static int DATABASE_VERSION = 1;
+
+    public SQLiteHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        String sqlCreateDB = "CREATE TABLE items(" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "title TEXT," +
+                "category TEXT," +
+                "price TEXT," +
+                "date TEXT)";
+        sqLiteDatabase.execSQL(sqlCreateDB);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+    }
+
+    public List<Item> getAll() {
+        List<Item> list = new ArrayList<>();
+        SQLiteDatabase sqLiteDatabase = getReadableDatabase();
+        String order = "date DESC";
+        Cursor rs = sqLiteDatabase.query("items",
+                null, null, null,
+                null, null, order);
+        while ((rs != null) && (rs.moveToNext())) {
+            int id = rs.getInt(0);
+            String title = rs.getString(1);
+            String category = rs.getString(2);
+            String price = rs.getString(3);
+            String date = rs.getString(4);
+            list.add(new Item(id, title, category, price, date));
+        }
+        return list;
+    }
+
+    public void addItem(Item i) {
+        String sql = "INSERT INTO items(title,category,price,date)" +
+                "VALUES(?,?,?,?)";
+        String[] args = {i.getTitle(), i.getCategory(), i.getPrice(), i.getDate()};
+        SQLiteDatabase st = getWritableDatabase();
+        st.execSQL(sql, args);
+    }
+}
--- app/src/main/java/com/example/kt2/fragment/FragmentHistory.java
+++ app/src/main/java/com/example/kt2/fragment/FragmentHistory.java
@@ -1,5 +1,6 @@
 package com.example.kt2.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,13 +9,57 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.kt2.R;
+import com.example.kt2.UpdateDeleteActivity;
+import com.example.kt2.adapter.RecycleViewAdapter;
+import com.example.kt2.dal.SQLiteHelper;
+import com.example.kt2.model.Item;
 
-public class FragmentHistory extends Fragment {
+import java.util.List;
+
+public class FragmentHistory extends Fragment implements RecycleViewAdapter.ItemListener {
+    private RecycleViewAdapter adapter;
+    private RecyclerView recyclerView;
+    private SQLiteHelper db;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_history, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.recycleView);
+        adapter = new RecycleViewAdapter();
+        db = new SQLiteHelper(getContext());
+        db.addItem(new Item("mua quanbo", "quan ao", "500k", "22/22/2022"));
+
+        List<Item> list = db.getAll();
+        adapter.setList(list);
+
+        LinearLayoutManager manager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setAdapter(adapter);
+        adapter.setItemListener(this);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        Item item = adapter.getItem(position);
+        Intent intent = new Intent(getActivity(), UpdateDeleteActivity.class);
+        intent.putExtra("item", item);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        List<Item> list = db.getAll();
+        adapter.setList(list);
     }
 }
